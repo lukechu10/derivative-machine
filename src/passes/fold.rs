1425d7dd0e197af007e2989fff0341b9725878bb
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-11-28 18:39:21 -0800

    Simplify fold algorithm

--- src/passes/fold.rs
+++ src/passes/fold.rs
@@ -2,32 +2,15 @@
 
 use crate::parser::{walk_expr, BinOpKind, Expr, ExprVisitor, UnaryOpKind};
 
-/// Fold literals (`1 + 1` becomes `2`).
+/// Runs one fold pass on the AST.
+/// The resulting AST may still be further folded. Keep running the pass until `last_pass_folded` remains false.
+/// Prefer using [`FoldVisitor`] instead.
 pub struct FoldVisitor;
 
 impl ExprVisitor for FoldVisitor {
     fn visit(&mut self, expr: &mut Expr) {
-        let mut visitor_internal = FoldVisitorInternal {
-            last_pass_folded: true,
-        };
+        walk_expr(expr, self);
 
-        while visitor_internal.last_pass_folded {
-            visitor_internal.last_pass_folded = false;
-            visitor_internal.visit(expr);
-        }
-    }
-}
-
-/// Runs one fold pass on the AST.
-/// The resulting AST may still be further folded. Keep running the pass until `last_pass_folded` remains false.
-/// Prefer using [`FoldVisitor`] instead.
-struct FoldVisitorInternal {
-    /// Whether the last pass folded any constants. The top level visit function should keep on running the pass until it is false.
-    last_pass_folded: bool,
-}
-
-impl ExprVisitor for FoldVisitorInternal {
-    fn visit(&mut self, expr: &mut Expr) {
         match expr {
             // fold multiplication / division with 0
             Expr::Binary {
@@ -35,7 +18,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Asterisk | BinOpKind::Slash,
                 right: _,
             } if *left == 0.0 => {
-                self.last_pass_folded = true;
                 *expr = Expr::Literal(0.0);
             }
             Expr::Binary {
@@ -43,7 +25,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Asterisk,
                 right: box Expr::Literal(right),
             } if *right == 0.0 => {
-                self.last_pass_folded = true;
                 *expr = Expr::Literal(0.0);
             }
             // fold addition with 0
@@ -52,7 +33,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Plus | BinOpKind::Minus,
                 right,
             } if *left == 0.0 => {
-                self.last_pass_folded = true;
                 *expr = *right.clone();
             }
             Expr::Binary {
@@ -60,7 +40,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Plus | BinOpKind::Minus,
                 right: box Expr::Literal(right),
             } if *right == 0.0 => {
-                self.last_pass_folded = true;
                 *expr = *left.clone();
             }
             // fold multiplication / division with 1
@@ -69,7 +48,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Asterisk,
                 right,
             } if *left == 1.0 => {
-                self.last_pass_folded = true;
                 *expr = *right.clone();
             }
             Expr::Binary {
@@ -77,7 +55,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Asterisk | BinOpKind::Slash,
                 right: box Expr::Literal(right),
             } if *right == 1.0 => {
-                self.last_pass_folded = true;
                 *expr = *left.clone();
             }
             // fold exponentiation with 1
@@ -86,7 +63,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Exponent,
                 right: box Expr::Literal(right),
             } if *right == 1.0 => {
-                self.last_pass_folded = true;
                 *expr = *left.clone();
             }
             // fold double exponent, e.g. (x ^ 2) ^ 3 = x ^ 6
@@ -100,7 +76,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op: BinOpKind::Exponent,
                 right: box Expr::Literal(outer),
             } => {
-                self.last_pass_folded = true;
                 *expr = Expr::Binary {
                     left: left.clone(),
                     op: BinOpKind::Exponent,
@@ -113,7 +88,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op,
                 right: box Expr::Literal(right_lit),
             } => {
-                self.last_pass_folded = true;
                 *expr = Expr::Literal(match op {
                     BinOpKind::Plus => *left_lit + *right_lit,
                     BinOpKind::Minus => *left_lit - *right_lit,
@@ -127,7 +101,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 op,
                 right: box Expr::Literal(right_lit),
             } => {
-                self.last_pass_folded = true;
                 *expr = Expr::Literal(match op {
                     UnaryOpKind::Plus => *right_lit,
                     UnaryOpKind::Minus => -*right_lit,
@@ -141,7 +114,6 @@ impl ExprVisitor for FoldVisitorInternal {
                 right: box Expr::Identifier(right_id),
             } => {
                 if left_id == right_id {
-                    self.last_pass_folded = true;
                     *expr = Expr::Binary {
                         left: Box::new(Expr::Literal(2.0)),
                         op: BinOpKind::Asterisk,
@@ -151,6 +123,5 @@ impl ExprVisitor for FoldVisitorInternal {
             }
             _ => {}
         }
-        walk_expr(expr, self);
     }
 }
