27472f5bb862fb5b97f482677e0a084c97fc3e69
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2022-10-23 22:32:12 +0100

    Update dependencies

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     console_error_panic_hook::set_once();
     wasm_logger::init(wasm_logger::Config::default());
 
-    sycamore::render(|ctx| sycamore::view! { ctx, app::App() });
+    sycamore::render(|cx| sycamore::view! { cx, app::App() });
 }
